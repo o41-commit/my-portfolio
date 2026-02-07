6911b307b8eb335fbf7b3c0a48ce40dc5e30dbe3
o41-commit <oluwatobilobafatoba@gmail.com>
2026-02-07 18:32:16 +0100

    Updated frontend for live backend

--- js/main.js
+++ js/main.js
@@ -31,3 +31,110 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: false,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// TO GET DATA FROM THE BACKEND
+const container = document.getElementById("data");
+
+async function testimonials() {
+  try {
+    const res = await fetch("https://portfoliodb-24w4.onrender.com/review");
+    const data = await res.json();
+
+    container.innerHTML = "";
+
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList.add("swiper-slide");
+      card.innerHTML = `
+  <div class="card">
+    <div class="head">
+      <h2 class="name">
+        Name: <span class="name-output">${item.name}</span>
+      </h2>
+      <h3 class="role">
+        Role: <span class="role-output">${item.role}</span>
+      </h3>
+    </div>
+    <div class="data">
+      <p class="data-output">
+        ${item.comment}
+      </p>
+    </div>
+  </div>
+`;
+
+      container.appendChild(card);
+    });
+
+    swiper.update();
+  } catch (error) {
+    console.error("Failed to fetch testimonials", error);
+  }
+}
+
+testimonials();
+
+// send a testimonials
+const form = document.getElementById("testimonial-form");
+const messageBox = document.querySelector(".mess");
+const respond = document.getElementById("respond");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const res = await fetch("https://portfoliodb-24w4.onrender.com/review", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await res.json();
+
+    respond.textContent = result.message;
+    messageBox.classList.add("show");
+
+    setTimeout(() => {
+      messageBox.classList.remove("show");
+    }, 3000);
+
+    form.reset();
+  } catch (error) {
+    respond.textContent = "Something went wrong. Please try again.";
+    messageBox.classList.add("show");
+  }
+});
